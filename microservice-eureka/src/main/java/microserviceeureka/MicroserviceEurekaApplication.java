bd2135b3da0e1c702c5a7a090029af0a12c67912
Pablo Nolasco Castro <pablo.nolasco@people-media.com.mx>
2022-09-29 19:06:13 -0500

    Feat: se agrego proyecto de eureka

---
+++ microservice-eureka/src/main/java/microserviceeureka/MicroserviceEurekaApplication.java
@@ -0,0 +1,13 @@
+package microserviceeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEurekaApplication.class, args);
+	}
+
+}
